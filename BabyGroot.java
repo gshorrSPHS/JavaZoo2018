0b26c3aaaa396fe369e42c9a6df96ed936180f22
Bubbleblops <37597501+Bubbleblops@users.noreply.github.com>
2018-03-22 09:29:56 -0700

    Jackson's animal classes

---
+++ BabyGroot.java
@@ -0,0 +1,10 @@
+public class BabyGroot extends Groot
+{
+    private int cuteness;
+    
+    public BabyGroot(int cuteness, String name, String desc)
+    {
+        super("Baby Groot", "A young form of the mysterious Groot", 10);
+        this.cuteness = cuteness;
+    }
+}
