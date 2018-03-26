7125e468b8bfd5ee532f55056e2a28640bcfd1ef
SirLancelotZ <ngtlancezhu@gmail.com>
2018-03-26 09:18:18 -0700

    Update PopcornyUnicorn.java

--- PopcornyUnicorn.java
+++ PopcornyUnicorn.java
@@ -4,7 +4,7 @@ public final class PopcornyUnicorn extends CornyUnicorn
     public PopcornyUnicorn()
     {
        super(10, "PopcornyUnicorn",
-           "This is a subgenre of the orny Unicorn. Instead of a corn, this unicorn uses a popcorn horn!"); 
+           "This is a subgenre of the corny Unicorn. Instead of a corn, this unicorn has a popcorn horn!"); 
     }
     
     @Override
