704e324e046fdd1e77b0bdfe4d06437a5f7e6141
Bubbleblops <37597501+Bubbleblops@users.noreply.github.com>
2018-03-26 09:25:16 -0700

    Update BabyGroot.java

--- BabyGroot.java
+++ BabyGroot.java
@@ -2,7 +2,7 @@ public class BabyGroot extends Groot
 {
     private int cuteness;
     
-    public BabyGroot(int cuteness, String name, String desc)
+    public BabyGroot(int cuteness)
     {
         super("Baby Groot", "A young form of the mysterious Groot", 10);
         this.cuteness = cuteness;
