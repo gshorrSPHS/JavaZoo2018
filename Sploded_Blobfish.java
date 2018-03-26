07d15dcf2c29569aadc3ad130b21bdfb4d2a33fe
EvanStrittmatter16 <37666670+EvanStrittmatter16@users.noreply.github.com>
2018-03-26 09:29:12 -0700

    Evan Strittmatter

--- Sploded_Blobfish.java
+++ Sploded_Blobfish.java
@@ -1,18 +1,9 @@
 
-/**
- * Write a description of class Sploded_Blobfish here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class Sploded_Blobfish extends Blobfish implements Swimming
 {
-    // instance variables - replace the example below with your own
     private int radius;
 
-    /**
-     * Constructor for objects of class Sploded_Blobfish
-     */
     public Sploded_Blobfish(int radius)
     {
         super(10, "Bill", "The blobfish has been brought to the surface and is exploding");
@@ -20,6 +11,10 @@ public class Sploded_Blobfish extends Blobfish implements Swimming
     }
     public String getSize(){
         return "The blob Fish has expanded to a size of " + radius + "feet!";
+    }
+    @Override
+    public String swim(){
+        return "But nothing happens...";
     }
    @Override
     public String eat(){
